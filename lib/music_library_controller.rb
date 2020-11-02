6ed496d41a31002b8f6ad09b4482f6c054afa342
Catherine Gloria <catherinegloria@gmail.com>
2020-11-02 22:53:46 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -77,6 +77,16 @@ class MusicLibraryController
     end
   end
 
-
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip
+    if input > 0 && input <= Song.all.length
+          array = Song.all.sort{|a, b| a.name <=> b.name}
+          song = array[input-1]
+          puts "Playing #{song.name} by #{song.artist.name}"
+        end
+      end
+      
+  end
 
 end
